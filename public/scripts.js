b5285e8eef07597bddc6cb71f023e5802ebd873c
giovanni-786 <giovanni.lima786@gmail.com>
2020-11-22 16:29:45 -0300

    recipe dinamico

--- public/scripts.js
+++ public/scripts.js
@@ -1,28 +1,60 @@
 const cardsRecipes = document.querySelectorAll('.card-recipe');
 
-const closeModal = document.querySelector('.close-modal');
+const hideAndShow_Ingredients = document.querySelector('.HideAndShow_Ingredients');
 
-const modalOverlay = document.querySelector('.modal-overlay')
+const hideAndShow_Preparation = document.querySelector('.HideAndShow_preparation');
 
+const hideAndShow_Information = document.querySelector('.HideAndShow_Information');
 
 
 
 for(let card of cardsRecipes){
     card.addEventListener('click', function(){
-        /*const recipeId = card.getAttribute("id");
-
-        const titleRecipes = card.querySelector('.card-recipe h3').innerHTML;
-        const AuthorRecipes = card.querySelector('.card-recipe p').innerHTML;
-
-        modalOverlay.classList.add('active');
-        document.querySelector('.modal-content h3').innerHTML = titleRecipes;
-        document.querySelector('.modal-content p').innerHTML = AuthorRecipes;
-
-        modalOverlay.querySelector("iframe").src = `/assets/${recipeId}.png`;
-        */
-
         const recipesIndex = card.getAttribute("id");
         window.location.href = `/recipes/${recipesIndex}`
 
     })
 }
+
+hideAndShow_Ingredients.addEventListener('click', function(){ 
+    const ingredients = document.querySelector('ul');
+    ingredients.classList.toggle('hide');
+
+    if(ingredients.className == "hide" ){
+        hideAndShow_Ingredients.innerHTML = "MOSTRAR"
+    }
+    else{
+        hideAndShow_Ingredients.innerHTML = "ESCONDER"
+    }
+})
+
+
+
+hideAndShow_Preparation.addEventListener('click', function(){ 
+    const preparation = document.querySelector('.preparation_mod_content div');
+    preparation.classList.toggle("hide")
+
+    if(preparation.className == "hide"){
+        hideAndShow_Preparation.innerHTML = "MOSTRAR"
+    }
+    else{
+        hideAndShow_Preparation.innerHTML = "ESCONDER"        
+    }
+   
+    
+})
+
+
+hideAndShow_Information.addEventListener('click', function(){ 
+    const information = document.querySelector('.informations_add_content p');
+    information.classList.toggle("hide")
+
+    if(information.className == "hide"){
+        hideAndShow_Information.innerHTML = "MOSTRAR"
+    }
+    else{
+        hideAndShow_Information.innerHTML = "ESCONDER"        
+    }
+   
+    
+})
